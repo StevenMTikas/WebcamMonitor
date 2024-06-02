24db49367a4be42a96464448db2e29ca28639408
Steve <stevenmtikas@gmail.com>
2024-06-02 17:59:47 -0400

    Program completed

---
+++ emailing.py
@@ -0,0 +1,27 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "wjkf yyha zgyi tkvt"
+SENDER = "stevenmtikas@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up"
+    email_message.set_content("Hey we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, SENDER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/clear.png")
+    
